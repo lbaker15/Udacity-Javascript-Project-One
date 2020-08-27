9cc920b7b078263d637f20e3247bd5c4b7ef41c2
lbaker15 <laelbaker@hotmail.co.uk>
2020-08-27 20:43:06 +0100

    27/08

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ class dinosaur {
 		this.height = height;
 		this.diet = diet;
 		this.img = img;
+		this.fact = function(array) {
+		  //array.sort(() => Math.random() - 0.5);
+		  return array;
+		}
 	}
+		
 		//Compare 1 - only other way could be to put in onclick and run functions in forEach loop to return new array
 		weightCompare() {
 			if (this.weight > humanObject.weight) {
@@ -39,13 +44,10 @@ class dinosaur {
 					let heightDif = humanFullHeight - this.height;
 					return `You are ${heightDif} inches taller than me!`
 				}	
-		}		
+		}
 }
 
-dinosaur.prototype.fact = function(array) {
-		  array.sort(() => Math.random() - 0.5);
-		  return array;
-	}
+
 
 
 
@@ -104,35 +106,36 @@ let dinoSix = new dinosaur("Elasmosaurus", 16000, 59, "carnivor", "elasmosaurus.
 let dinoSeven = new dinosaur("Pteranodon", 44, 20, "carnivor", "pteranodon.png")
 let dinoEight = new dinosaur("Pigeon", 0.5, 9, "Herbavor", "pigeon.png")
 
-//Fact Object
-facts = [
-	{ f: "First discovered in 1889 by Othniel Charles Marsh" },
-	{ f: "The largest known skull measures in at 5 feet long." },
-	{ f: "Anklyosaurus survived for approximately 135 million years." },
-	{ f:  "An asteroid was named 9954 Brachiosaurus in 1991." },
-	{ f: "The Stegosaurus had between 17 and 22 seperate places and flat spines." },
-	{ f: "Elasmosaurus was a marine reptile first discovered in Kansas." },
-	//Update
-	{ f:  "An asteroid was named 9954 Brachiosaurus in 1991." },
-	{ f: "The Stegosaurus had between 17 and 22 seperate places and flat spines." },
-	{ f: "Elasmosaurus was a marine reptile first discovered in Kansas." }	
-]
-pigeonFact = [{f: "All birds are living dinosaurs."}]
 
-/*
-document.querySelector('#a').addEventListener('click', loadData);
-
-function loadData() {
+//Fact Object calling via ajax
+btn.addEventListener('click', renderNew);
+(function loadData() {
 	const xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
 	if(this.status === 200 && this.readyState === 4){
-    document.getElementById('output').innerHTML = `<span>${this.responseText}</span>`;
+		return info = JSON.parse(this.responseText);	
     } 
   }
-  xhr.open("GET", "https://cors-anywhere.herokuapp.com/https:/localhost/Javascript/dino.json", true);
+  xhr.open("GET", "dino.json", true);
   xhr.send();
+})()
+
+//Pushing dino.json facts to seperate arrays for dinosaur and pigeon
+let facts = []
+let pigeonFact = []
+function renderNew() {
+	const a = info.Dinos.filter(x => x.fact !== 'All birds are living dinosaurs.')
+	const b = info.Dinos.filter(x => x.fact == 'All birds are living dinosaurs.')
+	a.forEach(function(element) {
+		facts.push(element.fact)
+	})
+	console.log(a)
+	//Because human object will be pushed to the dino array making number of items 9 - adding an extra fact onto the fact array NEED TO SLICE AND PUSH IN EMPTY FOR HUMAN
+	facts.push(a[0].fact)
+	pigeonFact.push(b[0].fact)
 }
-*/
+
+
 
 
 
@@ -140,7 +143,6 @@ function loadData() {
 btn.onclick = function() {
 	//Validation statement
 	if( name.value.length !== 0 ) {
-	(function () {
 
 
 		
@@ -152,13 +154,19 @@ btn.onclick = function() {
 	dinoSlice.push(dinoSliceTwo)
 	let dinoEnd = dinoSlice.flat()
 
-	
-	
+
+
+
 	 // Remove form from screen & create grid
 	const form = document.getElementById("dino-compare");
 	const grid = document.getElementById("grid");
 	form.style.display = "none";
+	
 	for(let i = 0; i < 9; i++) {
+		//Randomizer for methods
+		let methods = [dinoEnd[i].weightCompare.bind(dinoEnd[i]), dinoEnd[i].dietCompare.bind(dinoEnd[i]), dinoEnd[i].heightCompare.bind(dinoEnd[i])]
+		let j = Math.floor(Math.random()*3);
+		//Creating the grid template
 		const gridItem = document.createElement("div");
 		gridItem.classList.add("grid-item");
 		//Human Template
@@ -166,26 +174,21 @@ btn.onclick = function() {
 			gridItem.innerHTML = 
 	`<span class="padding"> <img src="${dinoEnd[i].img}"/> </span>
 	<span class="padding"> Species Name : ${dinoEnd[i].species} </span>  
-
 	<span class="padding"> Random fact : You are 100% human! </span>`	
 		} else if (dinoEnd[i].weight < 1 && dinoEnd[i].weight !== weightValue) {
 			gridItem.innerHTML = 
 	`<span class="padding"> <img src="${dinoEnd[i].img}"/> </span>
 	<span class="padding"> Species Name : ${dinoEnd[i].species} </span>  
-	<span class="padding"> Weight : ${dinoEnd[i].weightCompare()} </span>
-	<span class="padding"> Diet : ${dinoEnd[i].dietCompare()} </span>
-	<span class="padding"> Height : ${dinoEnd[i].heightCompare()} </span>
-	<span class="padding"> Random dino fact : ${dinoEnd[i].fact(pigeonFact)[0].f} </span>`	
+	<span class="padding"> Random fact: ${methods[j]()} </span>
+	<span class="padding"> Dino fact : ${dinoEnd[i].fact(pigeonFact)[0]} </span>`	
 		}	
 		//Dino Template
 		else {
 		gridItem.innerHTML = 
 	`<span class="padding"> <img src="${dinoEnd[i].img}"/> </span>
 	<span class="padding"> Species Name : ${dinoEnd[i].species} </span>  
-	<span class="padding"> Weight : ${dinoEnd[i].weightCompare()} </span>
-	<span class="padding"> Diet : ${dinoEnd[i].dietCompare()} </span>
-	<span class="padding"> Height : ${dinoEnd[i].heightCompare()} </span>
-	<span class="padding"> Random dino fact : ${dinoEnd[i].fact(facts)[i].f} </span>`	
+	<span class="padding"> Random fact: ${methods[j]()} </span>
+	<span class="padding"> Dino fact : ${dinoEnd[i].fact(facts)[i]} </span>`	
 		}	
 		grid.appendChild(gridItem);
 	}
@@ -193,7 +196,7 @@ btn.onclick = function() {
 
 
 	//Validation
-	})()} else {
+	} else {
 	const alert = document.createElement("div");
 	const main = document.querySelector(".box");
 	const form = document.getElementById("dino-compare");
